3c18d2b1a77151778b1a785ba2e696ab0a9e49cc
Emre Akay <emre.akay@plexusmind.com>
2022-01-30 16:45:02 +0300

    class example continue

---
+++ class-example/call_to_class.py
@@ -0,0 +1,6 @@
+import lesson1
+import lesson1 as emre
+
+if __name__ == '__main__':
+    lesson1.emre().zincir4()
+    emre.emre().zincir4()
---
+++ class-example/lesson1.py
@@ -0,0 +1,43 @@
+class emre:
+    ad = ''
+
+    def printName(self):
+        print('Merhaba Emre')
+
+    def kullaniciAdi(self):
+        ad = str(input('Adınızı Giriniz : '))
+        print('Merhaba ' + ad)
+
+    def zincir1(self):
+        self.ad = input('adınızı Giriniz : ')
+        return self
+
+    def zincir2(self):
+        if not type(self.ad) is str:
+            raise TypeError("Only string are allowed")
+        else:
+            return self
+
+    def zincir3(self):
+        print('adınız : ' + str(self.ad))
+        return self
+
+    def zincir4(self):
+        return self.zincir1().zincir2().zincir3()
+
+
+class ornek:
+    aa = ''
+
+    def __init__(self):
+        aa = 'asd'
+
+    def Başlangıcda_yazdir(self):
+        print(self.aa)
+
+    def hata_yazdir(self):
+        raise TypeError('Hata çıktıısıı')
+
+if __name__ == '__main__':
+    nesne = ornek()
+    nesne.hata_yazdir()

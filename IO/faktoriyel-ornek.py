7b85a67830c2ea6e6023dc07f6e74e63ad966d5d
Emre Akay <emre.akay@plexusmind.com>
2022-01-30 16:01:08 +0300

    example continue

---
+++ IO/faktoriyel-ornek.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    a = int(input('Faktoriyeli alınıcak sayıyı giriniz : ')) # burada input ile girilen verinin integer türüne cevap vermekte
+
+    b = 1
+    for i in range(1, a + 1):
+        b = b * i
+
+    print('sonuc :' + str(b))
+
+
+def fakt(x):
+
+    return 1\
+        if (x == 1 or x == 0)\
+        else x * fakt(x - 1)
+
+#sayi = int(input('Lütfen Faktöriyelini Hesaplamak İstediğini Sayını Giriniz : '))
+#print(str(sayi) + " Sayısının faktöriyeli  =  " + str(fakt(sayi)))
---
+++ IO/lesson1.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    a = input('isminizi Giriniz')
+    print( 'Merhaba ' + a)
